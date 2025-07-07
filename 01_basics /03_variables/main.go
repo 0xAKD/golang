900055914ca48727b4303526e3868e9d0f7c3184
heypolygot <helloajaydey@gmail.com>
2025-07-07 13:14:21 +0530

    variables in golang

---
+++ 01_basics /03_variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	var age int              //decalaration if an integer variable
+	var name string = "John" //declared a string variable and assigned a value.
+
+	/* mentioning datatype can be skipped if we assign the value at the time of declaration. */
+	var name1 = "James"
+
+	count := 10 //direct assignment of value without var keyword and datatype
+	lastName := "Wick"
+
+	// Note: The ':=' short variable declaration can only be used inside functions.
+	// It cannot be used with the 'var' keyword or when specifying an explicit data type.
+
+}
