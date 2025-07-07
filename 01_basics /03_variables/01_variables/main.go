3125813ebf2b3baa8e79f33600a33e680ac282c6
heypolygot <helloajaydey@gmail.com>
2025-07-07 13:37:13 +0530

    constants in go

--- 01_basics /03_variables/01_variables/main.go
+++ 01_basics /03_variables/01_variables/main.go
@@ -1,8 +1,12 @@
 package main
 
+var middleName = "Dennis" //Global variable
+
 func main() {
 	var age int              //decalaration if an integer variable
 	var name string = "John" //declared a string variable and assigned a value.
+
+	middleName := "Micheal" //changing the value of global variable from inside of a function.
 
 	/* mentioning datatype can be skipped if we assign the value at the time of declaration. */
 	var name1 = "James"
@@ -11,6 +15,6 @@ func main() {
 	lastName := "Wick"
 
 	// Note: The ':=' short variable declaration can only be used inside functions.
-	// It cannot be used with the 'var' keyword or when specifying an explicit data type.
+	// It cannot be used with the 'var' keyword or when specifying a n explicit data type.
 
 }
---
+++ 01_basics /03_variables/02_constaints/main.go
@@ -0,0 +1,16 @@
+package main
+
+const PI = 3.14
+const GRAVITY = 9.81
+
+func main() {
+	const days int = 7
+
+	//const block
+	const (
+		date  = 20
+		month = 01
+		year  = 2005
+	)
+
+}
