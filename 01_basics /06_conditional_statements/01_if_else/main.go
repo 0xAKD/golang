cdb3059159dbf82eb7b2d298ae3a334a0b513d59
heypolygot <helloajaydey@gmail.com>
2025-07-12 09:43:42 +0530

    Conditional statements - if-else

---
+++ 01_basics /06_conditional_statements/01_if_else/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// simple example of if-else statement
+	num := 15
+	if num > 10 {
+		fmt.Println("True")
+	} else {
+		fmt.Println("False")
+	}
+
+	// nested if statements
+	if num > 10 {
+		if num > 12 {
+			fmt.Println("Both conditions are true")
+		} else {
+			fmt.Println("Only the first condition is true")
+		}
+	} else {
+		fmt.Println("None of the conditions are true")
+	}
+
+	// && AND
+	if num > 10 && num < 20 {
+		fmt.Println("Both conditions are true")
+	} else {
+		fmt.Println("False")
+	}
+
+	// || OR
+	if num > 10 || num < 20 {
+		fmt.Println("One of the conditions are true")
+	} else {
+		fmt.Println("False")
+	}
+
+	// Else-if
+	if num < 10 {
+		fmt.Println("First condition is true")
+	} else if num > 12 {
+		fmt.Println("Second Condition is true")
+	} else {
+		fmt.Println("None of the above conditions are true")
+	}
+
+}
